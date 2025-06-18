0b84855a94f831d5bc30b00d7efe8195a32e600b
areif-dev <aj@ajreifsnyder.com>
2025-06-17 20:22:17 -0400

    Create seperate crate for working with ABC

---
+++ abc-integration/src/main.rs
@@ -0,0 +1,84 @@
+mod product;
+
+use clap::Parser;
+use generator::Label;
+
+#[derive(Parser)]
+struct Cli {
+    /// Path the the TSV file containing the data for ABC report 2-16 (Bill Details)
+    #[arg(
+        short,
+        long,
+        default_value = "C:\\Users\\User\\Documents\\My ABC Files\\TabOutput.tsv"
+    )]
+    tabfile: String,
+
+    /// Path to the ABC Item Detail file obtained by running report 7-10 (SQL Export)
+    #[arg(
+        short,
+        long,
+        default_value = "C:\\ABC Software\\Database Export\\Company001\\Data\\item_detail.data"
+    )]
+    detail_file: String,
+
+    /// Path to the ABC Item Posted Data file obtained by running report 7-10 (SQL Export)
+    #[arg(
+        short,
+        long,
+        default_value = "C:\\ABC Software\\Database Export\\Company001\\Data\\item_posted.data"
+    )]
+    posted_file: String,
+}
+
+pub fn read_216(tabfile: &str) -> Result<Vec<String>, String> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b'\t')
+        .flexible(true)
+        .has_headers(false)
+        .from_path(tabfile)
+        .or_else(|e| Err(format!("Can't open {} due to {}", tabfile, e)))?;
+
+    let mut skus = Vec::new();
+    for (i, result) in rdr.records().skip(2).enumerate() {
+        // ABC 2-16 report splits bills at every 100 items and inserts a new header. Always skip
+        // that header
+        if i > 0 && i % 100 == 0 {
+            continue;
+        }
+
+        let record =
+            result.or_else(|e| Err(format!("Bad row in bill TabOutput caused by {}", e)))?;
+        let sku = match record.get(1) {
+            Some(s) => s,
+            None => {
+                continue;
+            }
+        };
+        if sku == "" {
+            continue;
+        }
+        skus.push(sku.to_string());
+    }
+    Ok(skus)
+}
+
+fn main() -> Result<(), String> {
+    let cli = Cli::parse();
+    let skus = read_216(&cli.tabfile)?;
+    let all_products = product::parse_abc_item_files(&cli.detail_file, &cli.posted_file)
+        .or_else(|e| Err(format!("Cannot read ABC inventory export due to {}", e)))?;
+    let (good_skus, failed_skus): (Vec<String>, Vec<String>) = skus
+        .into_iter()
+        .partition(|sku| all_products.contains_key(sku));
+    if failed_skus.len() > 0 {
+        eprintln!(
+            "The following skus were not found in the ABC Inventory Export: {:?}",
+            failed_skus
+        );
+    }
+    let labels: Vec<Label> = good_skus.into_iter().map(|sku| {
+        let product = all_products.get(&sku).unwrap();
+        Label::new().with_sku(&product.sku()).with_desc(&product.desc()).with_price(product.list())
+    })
+    Ok(())
+}
